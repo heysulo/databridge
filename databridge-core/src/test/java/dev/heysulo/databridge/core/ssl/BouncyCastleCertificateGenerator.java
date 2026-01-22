6766412e83f76d094ea41e0e1cfa52e7711504d5
Sulochana Kodituwakku <sulochana.456@live.com>
2026-01-22 20:05:20 +0100

    Fix: Replace deprecated X509V3CertificateGenerator

--- databridge-core/src/test/java/dev/heysulo/databridge/core/ssl/BouncyCastleCertificateGenerator.java
+++ databridge-core/src/test/java/dev/heysulo/databridge/core/ssl/BouncyCastleCertificateGenerator.java
@@ -1,7 +1,11 @@
 package dev.heysulo.databridge.core.ssl;
 
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.x509.X509V3CertificateGenerator;
 
 import javax.security.auth.x500.X500Principal;
 import java.math.BigInteger;
@@ -24,14 +28,26 @@ public class BouncyCastleCertificateGenerator {
     }
 
     public static X509Certificate generateCertificate(KeyPair keyPair) throws Exception {
-        X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
-        certGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
-        certGen.setIssuerDN(new X500Principal("CN=Test Certificate"));
-        certGen.setNotBefore(new Date(System.currentTimeMillis()));
-        certGen.setNotAfter(new Date(System.currentTimeMillis() + 365 * 24 * 60 * 60 * 1000L));
-        certGen.setSubjectDN(new X500Principal("CN=localhost"));
-        certGen.setPublicKey(keyPair.getPublic());
-        certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
-        return certGen.generateX509Certificate(keyPair.getPrivate(), "BC");
+        X500Principal subject = new X500Principal("CN=localhost");
+        X500Principal issuer = new X500Principal("CN=Test Certificate");
+        Date notBefore = new Date(System.currentTimeMillis());
+        Date notAfter = new Date(System.currentTimeMillis() + 365 * 24 * 60 * 60 * 1000L);
+        BigInteger serialNumber = BigInteger.valueOf(System.currentTimeMillis());
+
+        X509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
+                issuer,
+                serialNumber,
+                notBefore,
+                notAfter,
+                subject,
+                keyPair.getPublic());
+
+        ContentSigner contentSigner = new JcaContentSignerBuilder("SHA256WithRSAEncryption")
+                .setProvider("BC")
+                .build(keyPair.getPrivate());
+
+        return new JcaX509CertificateConverter()
+                .setProvider("BC")
+                .getCertificate(certBuilder.build(contentSigner));
     }
 }
